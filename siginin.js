e107e8e253309a9fe0d514ae2df9c577707a3c13
RiabtsevaAnne <hannariabtseva05@gmail.com>
2023-03-17 21:03:51 +0200

    project6

---
+++ siginin.js
@@ -0,0 +1,6 @@
+alert("'${name} ${surname}', nice to see you")
+
+const email = document.getElementsByName("email")[0];
+const password = document.getElementsByName("password")[0];
+
+console.log(email.value, pawssword.value)
