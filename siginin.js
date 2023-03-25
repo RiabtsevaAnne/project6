a8a7f7be4afc2f21caf64c55ea4880ff5c8b4abb
RiabtsevaAnne <hannariabtseva05@gmail.com>
2023-03-25 11:31:13 +0200

    project6

--- siginin.js
+++ siginin.js
@@ -3,4 +3,12 @@ alert("'${name} ${surname}', nice to see you")
 const email = document.getElementsByName("email")[0];
 const password = document.getElementsByName("password")[0];
 
-console.log(email.value, pawssword.value)
+form.onsubmit = (e) => {
+    e.preventDefault();
+    console.log(email.value, password.value);
+  };
+
+if(someField.length < 1) {
+    alert("Будь ласка заповніть поле")
+}
+
